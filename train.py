e38e5b88be3f384d33d2e78ed6b23c1da3b0560b
Tom <tmbdev@varuna>
2021-04-13 11:56:13 -0700

    updated model

--- train.py
+++ train.py
@@ -95,13 +95,23 @@ class ImagenetData(pl.LightningDataModule):
     def val_dataloader(self):
         return self.make_loader(self.val_urls, mode="val")
 
+    @staticmethod
+    def add_loader_specific_args(parser):
+        parser.add_argument("-b", "--batch-size", type=int, default=128)
+        parser.add_argument("--bucket", default="./shards")
+        parser.add_argument("--shards", default="imagenet-train-{000000..000146}.tar")
+        parser.add_argument("--valshards", default="imagenet-val-{000000..000006}.tar")
+        return parser
+
 
 class ImageClassifier(pl.LightningModule):
 
-    def __init__(self, learning_rate=0.1):
+    def __init__(self, learning_rate=0.1, model="resnet18"):
         super().__init__()
-        self.model = torchvision.models.resnet18()
+        self.save_hyperparameters()
         self.learning_rate = learning_rate
+        torchvision
+        self.model = eval(f"torchvision.models.{model}")()
 
     def forward(self, inputs):
         return self.model(inputs)
@@ -111,17 +121,28 @@ class ImageClassifier(pl.LightningModule):
         output = self(x)
         loss = F.cross_entropy(output, y)
         err = (output.argmax(1) != y).sum() / float(len(y))
+        self.log('train_loss', loss, on_step=True, on_epoch=True, sync_dist=True)
+        self.log('train_err', err, on_step=True, on_epoch=True, sync_dist=True)
         return dict(loss=loss, err=err)
 
     def validation_step(self, batch, batch_idx):
         return self.training_step(batch, batch_idx)
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.01)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.learning_rate, momentum=0.9)
+        schedule = torch.optim.lr_scheduler.MultiStepLR(optimizer, [10, 50, 100, 150, 200], gamma=0.1)
+        # optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        # schedule = torch.optim.lr_scheduler.MultiStepLR(optimizer, [10, 50, 100, 150, 200], gamma=0.1)
+        return [optimizer], [schedule]
+
+    @staticmethod
+    def add_model_specific_args(parser):
+        parser.add_argument("--learning_rate", type=float, default=0.1)
+        return parser
 
 
-def main(hparams):
-    model = ImageClassifier()
+def main(args):
+    model = ImageClassifier(learning_rate=args.learning_rate)
     plugin = plugins.DDPPlugin(find_unused_parameters=False)
     trainer = pl.Trainer.from_argparse_args(args, plugins=plugin)
     data = ImagenetData(batch_size=args.batch_size, training_urls=args.shards, val_urls=args.valshards, bucket=args.bucket)
@@ -130,12 +151,8 @@ def main(hparams):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-b", "--batch-size", type=int, default=128)
-    parser.add_argument("--bucket", default="./shards")
-    parser.add_argument("--shards", default="imagenet-train-{000000..000489}.tar")
-    parser.add_argument("--valshards", default="imagenet-val-{000000..000022}.tar")
-    parser = pl.Trainer.add_argparse_args(
-        parser.add_argument_group(title="pl.Trainer args")
-    )
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = ImagenetData.add_loader_specific_args(parser)
+    parser = ImageClassifier.add_model_specific_args(parser)
     args = parser.parse_args()
     main(args)
