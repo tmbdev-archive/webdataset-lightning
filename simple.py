3d4f1ea48d57d76b1fc7edda68f0bc4911f2341c
Tom <tmbdev+github@gmail.com>
2021-04-13 16:32:54 -0700

    testcase

--- simple.py
+++ simple.py
@@ -1,6 +1,5 @@
-import sys
 import torch
 
 print("init")
 torch.distributed.init_process_group("gloo")
-print("done", torch.ditsributed.get_rank(), torch.distributed.get_world_size())
+print("done", torch.distributed.get_rank(), torch.distributed.get_world_size())
